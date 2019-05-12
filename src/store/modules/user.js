8b32aa3138fa50511490b442a278436c9aff039b
fanqilongmoli <fanqilong_moli@163.com>
2019-05-12 23:49:56 +0800

    <M> 修改登出逻辑

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -70,15 +70,21 @@ const user = {
 
     // 登出
     LogOut({ commit, state }) {
+      // return new Promise((resolve, reject) => {
+      //   logout(state.token).then(() => {
+      //     commit('SET_TOKEN', '')
+      //     commit('SET_ROLES', [])
+      //     removeToken()
+      //     resolve()
+      //   }).catch(error => {
+      //     reject(error)
+      //   })
+      // })
       return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
           commit('SET_TOKEN', '')
           commit('SET_ROLES', [])
           removeToken()
           resolve()
-        }).catch(error => {
-          reject(error)
-        })
       })
     },
 
