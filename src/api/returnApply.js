8d02851bf80c5a175a909303ce0e16206c044ae5
fanqilongmoli <fanqilong_moli@163.com>
2019-06-02 22:00:13 +0800

    <A> 退货还没有修改

--- src/api/returnApply.js
+++ src/api/returnApply.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 export function fetchList(params) {
   return request({
     url:'/returnApply/list',
-    method:'get',
+    method:'post',
     params:params
   })
 }
